cfb335eb3050eee24c9901b84dacde09aed8a991
pierre-slimm <mainapeter008@gmail.com>
2025-01-12 17:24:24 +0300

    stule

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Database configuration
-define('DB_HOST', 'localhost');          // Database host
-define('DB_PORT', '5432');               // PostgreSQL port, default is 5432
-define('DB_NAME', 'submission_submit'); // Database name
-define('DB_USER', 'postgres');      // Database username
-define('DB_PASS', 'Slimmworld25');      // Database password
-?>
